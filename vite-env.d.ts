918c36c23ec22a39d8892733f1b7e88f20befd1a
IsakovaDM <darinaisakova@gmail.com>
2025-12-15 18:12:51 +0500

    add newlines

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -2,3 +2,6 @@ declare module '*.hbs?raw' {
   const content: string;
   export default content;
 }
+
+
+
--- vite.config.js
+++ vite.config.js
@@ -13,3 +13,6 @@ export default defineConfig({
         postcss: './postcss.config.js',
     },
 });
+
+
+
